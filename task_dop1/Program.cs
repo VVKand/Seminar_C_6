087b404c80a2c88b3c79baf833081614680cf515
vkand <vvkand@mail.ru>
2023-01-15 20:41:43 +0700

    решил 2 доп заддания

--- task_dop1/Program.cs
+++ task_dop1/Program.cs
@@ -5,33 +5,37 @@
 
 
 Console.WriteLine("Введите стороны a, b, c. Числа должны быть положительными:");
-int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-int c = Convert.ToInt32(Console.ReadLine());
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+double c = Convert.ToInt32(Console.ReadLine());
 
 if (a + b <= c || b + c <= a || a + c <= b) Console.WriteLine("Невозможно построить треугольник с введенными сторонами");
 else
 {
-    string[] array = new string[3];
-    Gradus(array);
-    SP(array);
+    double[] ugly = new double[3];
+    Gradus(ugly);
+    SP(ugly);
 }
 
 
-void Gradus(string[] array)
+void Gradus(double[] array)
 {
     Console.WriteLine("Углы: ");
-    double A = Math.Acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / Math.PI;
+    double w1 =  Math.Acos((b * b + c * c - a * a) / (2 * b * c));
+    double A = w1 * 180 / Math.PI;
+
     Console.WriteLine($"Угол A = {A} градусов");
-    double B = Math.Acos((a * a + c * c - b * b) / (2 * a * c)) * 180 / Math.PI;
+    double w2 = Math.Acos((a * a + c * c - b * b) / (2 * a * c));
+    double B = w2 * 180 / Math.PI;
     Console.WriteLine($"Угол B = {B} градусов");
-    double C = Math.Acos((a * a + b * b - c * c) / (2 * a * b)) * 180 / Math.PI;
+    double w3 = Math.Acos((a * a + b * b - c * c) / (2 * a * b));
+    double C = w3 * 180 / Math.PI;
     Console.WriteLine($"Угол C = {C} градусов");
     if (A == 90 || B == 90 || C == 90) Console.WriteLine("Треугольник является прямоугольным");
 }
 
 
-void SP(string[] array)
+void SP(double[] array)
 {
     if (a == b || a == c || b == c) Console.WriteLine("Треугольник равнобедренный");
     if (a == b && a == c && b == c) Console.WriteLine("Треугольник равноcторонний");
