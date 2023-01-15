0f80457a8b7817cc4d8ecdb80ff7f2663ed9573a
vkand <vvkand@mail.ru>
2023-01-15 14:56:36 +0700

    выполнил две доп задачи

---
+++ task_dop1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 40 - HARD необязательная. На вход программы подаются три целых положительных числа. 
+// Определить , является ли это сторонами треугольника. Если да, то вывести всю информацию 
+// по нему - площадь, периметр, значения углов треугольника в градусах, является ли он 
+// прямоугольным, равнобедренным, равносторонним.
+
+
+Console.WriteLine("Введите стороны a, b, c. Числа должны быть положительными:");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a + b <= c || b + c <= a || a + c <= b) Console.WriteLine("Невозможно построить треугольник с введенными сторонами");
+else
+{
+    string[] array = new string[3];
+    Gradus(array);
+    SP(array);
+}
+
+
+void Gradus(string[] array)
+{
+    Console.WriteLine("Углы: ");
+    double A = Math.Acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / Math.PI;
+    Console.WriteLine($"Угол A = {A} градусов");
+    double B = Math.Acos((a * a + c * c - b * b) / (2 * a * c)) * 180 / Math.PI;
+    Console.WriteLine($"Угол B = {B} градусов");
+    double C = Math.Acos((a * a + b * b - c * c) / (2 * a * b)) * 180 / Math.PI;
+    Console.WriteLine($"Угол C = {C} градусов");
+    if (A == 90 || B == 90 || C == 90) Console.WriteLine("Треугольник является прямоугольным");
+}
+
+
+void SP(string[] array)
+{
+    if (a == b || a == c || b == c) Console.WriteLine("Треугольник равнобедренный");
+    if (a == b && a == c && b == c) Console.WriteLine("Треугольник равноcторонний");
+    double P = a + b + c; //периметр треугольника
+    double p = P / 2; //полупериметр треугольника
+    double S = Math.Sqrt(p * (p - a) * (p - b) * (p - c)); //площадь треугольника
+    Console.WriteLine($"Периметр треугольника = {P}");
+    Console.WriteLine($"Площадь треугольника = {S}");
+}
---
+++ task_dop2/Program.cs
@@ -0,0 +1,77 @@
+﻿// задача 2 HARD необязательная. Сгенерировать массив случайных целых чисел размерностью 
+// m*n (размерность вводим с клавиатуры). Вывести на экран красивенько таблицей. Найти 
+// минимальное число и его индекс, найти максимальное число и его индекс. Вывести эту информацию на экран.
+
+Console.Write($"Введите количество строк массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+            for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(10, 100);//[1; 10)
+        }
+    }
+}
+
+void MinMax(int[,] matr)
+{
+    int indmax0 = 0, indmax1 = 0, maxmatr = 0; 
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            
+            if (maxmatr < matr[i, j])
+            {
+                 maxmatr = matr[i, j];
+                 indmax1 = j;
+                 indmax0 = i;
+            }
+            
+        }        
+    }
+        
+    int indmin0 = 0, indmin1 = 0, minmatr = maxmatr;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            
+            if (minmatr > matr[i, j]) 
+            {
+                minmatr = matr[i, j];
+                indmin0 = i;
+                indmin1 = j;
+            }
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"min = {minmatr} в ячейке [{indmin0}, {indmin1}]");
+    Console.WriteLine($"max = {maxmatr} в ячейке [{indmax0}, {indmax1}]");
+    return;
+}
+
+int[,] matrix = new int[a, b];
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+MinMax(matrix);
